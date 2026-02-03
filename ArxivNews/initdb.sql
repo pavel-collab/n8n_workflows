7d2aa6ec8bc75fe431b3bfeaafc05e93366bb9a1
Pavel Filippenko <pavelfilippenko@Pavels-MacBook-Air.local>
2026-02-03 19:16:13 +0300

    add database initial sql scripts to create a tables when we start containers

---
+++ ArxivNews/initdb.sql
@@ -0,0 +1,8 @@
+-- maybe we don't need to create a database if we set an enviroment variable in docker-compose
+CREATE DATABASE arxiv_records;
+
+\c arxiv_records
+
+create type status_type as enum ('new', 'queued', 'shown');
+
+CREATE TABLE arxiv_records (title text primary key, link text, author text, pub_date timestamptz not null, summary text not null, status status_type not null);
---
+++ KaggleCompetitions/initdb.sql
@@ -0,0 +1,8 @@
+-- maybe we don't need to create a database if we set an enviroment variable in docker-compose
+CREATE DATABASE kaggle_competitions;
+
+\c kaggle_competitions
+
+create type status_type as enum ('new', 'queued', 'shown');
+
+CREATE TABLE kaggle_competitions (title TEXT PRIMARY KEY, link TEXT NOT NULL, date_start TIMESTAMPTZ NOT NULL, deadline TIMESTAMPTZ NOT NULL, status status_type NOT NULL);
